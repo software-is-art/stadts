fc23d6369b9b92f1db229c0338b4d52212333a16
software-is-art <37229146+software-is-art@users.noreply.github.com>
2023-05-29 09:22:03 +1200

    WIP

---
+++ StaDTs.Benchmarks/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Dunet;
+
+BenchmarkRunner.Run<StaDTsBenchmarks>();
+
+[Union]
+public partial record Shape
+{
+    public partial record Circle(double Radius);
+    public partial record Rectangle(double Length, double Width);
+    public partial record Triangle(double Base, double Height);
+}
+
+[MemoryDiagnoser]
+public class StaDTsBenchmarks
+{
+
+    [Benchmark]
+    public double Dunet() => DunetArea(new Shape.Circle(10));
+
+    [Benchmark]
+    public double Stadts() => Stadts(new StaDTs.Example.Shape.Circle(10));
+
+    private static double DunetArea(Shape shape) => shape.Match(
+            c => c.Radius,
+            r => r.Length,
+            t => t.Base
+        );
+
+    private static double Stadts<TShape>(in TShape shape) where TShape : StaDTs.Example.Shape<TShape> {
+        return TShape.Match(
+            in shape,
+            (in circle) => circle.Radius,
+            Rectangle,
+            Triangle
+        );
+        T Circle<T>(in Shape.Circle circle) where T : double => circle.Radius;
+        double Rectangle(in Shape.Rectangle rectangle) => rectangle.Length;
+        double Triangle(in Shape.Triangle triangle) => triangle.Base;
+     }
+}
+
---
+++ StaDTs.Example/Class1.cs
@@ -0,0 +1,131 @@
+﻿
+
+using static StaDTs.Example.Shape;
+
+namespace StaDTs.Example;
+
+[Union]
+public abstract partial record Shape
+{
+    partial record struct Circle(double Radius);
+    partial record struct Rectangle(double Length, double Width);
+    partial record struct Triangle(double Base, double Height);
+}
+
+public delegate void CircleAction(in Circle value);
+public delegate TResult CircleFunc<TResult>(in Circle value);
+
+public delegate void RectangleAction(in Rectangle value);
+public delegate TResult RectangleFunc<TResult>(in Rectangle value);
+
+public delegate void TriangleAction(in Triangle value);
+public delegate TResult TriangleFunc<TResult>(in Triangle value);
+
+public partial interface Shape<TMatch> where TMatch : Shape<TMatch>
+{
+    public static abstract void Match(in TMatch match, CircleAction circle, RectangleAction rectangle, TriangleAction triangle);
+
+    public static abstract TResult Match<TResult>(in TMatch match, CircleFunc<TResult> circle, RectangleFunc<TResult> rectangle, TriangleFunc<TResult> triangle);
+}
+
+
+public abstract partial record Shape
+{
+    public abstract void Match(CircleAction circle, RectangleAction rectangle, TriangleAction triangle);
+    public abstract TResult Match<TResult>(CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle);
+
+    private record CircleBoxed : Shape
+    {
+        private readonly Circle _circle;
+        public CircleBoxed(in Circle Circle)
+        {
+            _circle = Circle;
+        }
+        public override void Match(CircleAction circle, RectangleAction rectangle, TriangleAction triangle)
+        {
+            circle(in _circle);
+        }
+
+        public override TResult Match<TResult>(CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle)
+        {
+            return circle(in _circle);
+        }
+    }
+    private record RectangleBoxed : Shape
+    {
+        private readonly Rectangle _rectangle;
+        public RectangleBoxed(in Rectangle Rectangle)
+        {
+            _rectangle = Rectangle;
+        }
+        public override void Match(CircleAction circle, RectangleAction rectangle, TriangleAction triangle)
+        {
+            rectangle(in _rectangle);
+        }
+
+        public override TResult Match<TResult>(CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle)
+        {
+            return rectangle(in _rectangle);
+        }
+    }
+    private record TriangleBoxed : Shape
+    {
+        private readonly Triangle _triangle;
+        public TriangleBoxed(in Triangle Triangle)
+        {
+            _triangle = Triangle;
+        }
+        public override void Match(CircleAction circle, RectangleAction rectangle, TriangleAction triangle)
+        {
+            triangle(in _triangle);
+        }
+
+        public override TResult Match<TResult>(CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle)
+        {
+            return triangle(in _triangle);
+        }
+    }
+
+    public partial record struct Circle : Shape<Circle>
+    {
+        public static void Match(in Circle match, CircleAction circle, RectangleAction rectangle, TriangleAction triangle)
+        {
+            circle(in match);
+        }
+
+        public static TResult Match<TResult>(in Circle match, CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle)
+        {
+            return circle(in match);
+        }
+
+        public static implicit operator Shape(Circle circle) => new CircleBoxed(circle);
+    }
+    public partial record struct Rectangle : Shape<Rectangle>
+    {
+        public static void Match(in Rectangle match, CircleAction circle, RectangleAction rectangle, TriangleAction triangle)
+        {
+            rectangle(in match);
+        }
+
+        public static TResult Match<TResult>(in Rectangle match, CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle)
+        {
+            return rectangle(in match);
+        }
+    }
+    public partial record struct Triangle : Shape<Triangle>
+    {
+        public static void Match(in Triangle match, CircleAction circle, RectangleAction rectangle, TriangleAction triangle)
+        {
+            triangle(in match);
+        }
+
+        public static TResult Match<TResult>(in Triangle match, CircleFunc<TResult> circle, RectangleFunc<TResult>rectangle, TriangleFunc<TResult> triangle)
+        {
+            return triangle(in match);
+        }
+    }
+}
+
+ 
+
+
